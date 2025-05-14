74063e0d4bb7e35ed67963c8ee8917070d9a8aa7
depesh1977 <46925183+depesh1977@users.noreply.github.com>
2025-05-14 06:58:18 +0300

    Update TW_flags.mk

--- TW_flags.mk
+++ TW_flags.mk
@@ -11,7 +11,7 @@ RELAX_USES_LIBRARY_CHECK=true
 TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_BRIGHTNESS_PATH := "/sys/devices/platform/soc/soc:ap-ahb/20400000.dsi/20400000.dsi.0/display/panel0/sprd_backlight/brightness"
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/sprd_backlight/brightness"
 TW_INCLUDE_FASTBOOTD := true
 TW_INCLUDE_NTFS_3G := true
 TW_USE_TOOLBOX := true
@@ -24,8 +24,6 @@ TW_NO_SCREEN_BLANK := true
 TW_NO_LEGACY_PROPS := true
 TW_OVERRIDE_SYSTEM_PROPS := "ro.build.version.sdk"
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
-TW_DEFAULT_BRIGHTNESS := 500
-TW_MAX_BRIGHTNESS := 4000
 
 # resetprop & repacktools
 TW_INCLUDE_RESETPROP := true
